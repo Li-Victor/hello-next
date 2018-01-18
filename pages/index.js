27a153eecb1539c0cba68695d2d20ff04cb880ea
Victor Li <victorli5674@yahoo.com>
2018-01-18 10:26:06 -0800

    Creating Dynamic Pages

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import Layout from '../components/MyLayout';
 
-const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a href={`/post?title=${props.title}`}>{props.title}</a>
+    </Link>
+  </li>
 );
 
-export default Index;
+PostLink.propTypes = {
+  title: PropTypes.string.isRequired
+};
+
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink title="Hello Next.js" />
+      <PostLink title="Learn Next.js is awesome" />
+      <PostLink title="Deploy apps with Zeit" />
+    </ul>
+  </Layout>
+);
