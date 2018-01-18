0890aa602a13c8771c86723812f8677de51f8074
Victor Li <victorli5674@yahoo.com>
2018-01-18 11:09:27 -0800

    Fetching Data for Pages

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
-import PropTypes from 'prop-types';
+import fetch from 'isomorphic-unfetch';
 import Layout from '../components/MyLayout';
 
-const PostLink = props => (
-  <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
-      <a href={`/post?title=${props.title}`}>{props.title}</a>
-    </Link>
-  </li>
-);
-
-PostLink.propTypes = {
-  title: PropTypes.string.isRequired
-};
-
-export default () => (
+const Index = props => (
   <Layout>
-    <h1>My Blog</h1>
+    <h1>Batman TV Shows</h1>
     <ul>
-      <PostLink id="hello-nextjs" title="Hello Next.js" />
-      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
-      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+      {props.shows.map(({ show }) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a href={`/post?id=${show.id}`}>{show.name}</a>
+          </Link>
+        </li>
+      ))}
     </ul>
   </Layout>
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data
+  };
+};
+
+export default Index;
